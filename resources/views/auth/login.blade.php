5de3d041328ca448e3443649c88ae87e60e0feea
hellokirana <intaniasyahla@gmail.com>
2025-07-15 09:54:40 +0700

    FIX - php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,7 +55,7 @@
                         </div>
 
                         <!-- CAPTCHA Section -->
-                        <div class="row mb-3">
+                        <div class="mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}"></div>
                                 @error('g-recaptcha-response')
